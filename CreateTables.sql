78c3ef9dad969c2e2cd028f2f4cbd7333f6e3e9c
Ife <sunmolaife@gmail.com>
2022-05-17 02:14:55 -0600

    changed IDs/name to be the same in lecture slides

--- CreateTables.sql
+++ CreateTables.sql
@@ -1,5 +1,5 @@
 USE LectureDb;
-
+-- order of dropping matters
 DROP TABLE IF EXISTS Takes;
 DROP TABLE IF EXISTS Teaches;
 DROP TABLE IF EXISTS Students;
@@ -11,22 +11,22 @@ DROP TABLE IF EXISTS Department;
 
 CREATE TABLE Department(
 	dept_name VARCHAR(20) PRIMARY KEY,
-	building VARCHAR(20) NOT NULL, -- made building a primary key so it can be used in Section
+	building VARCHAR(20) NOT NULL,
 	budget NUMERIC(8, 2),
 );
 
 
 CREATE TABLE Students(
-	student_id CHAR(5) PRIMARY KEY,
-	student_name VARCHAR(20) NOT NULL,
+	ID CHAR(5) PRIMARY KEY,
+	[name] VARCHAR(20) NOT NULL,
 	dept_name VARCHAR(20) NOT NULL,
 	tot_credit NUMERIC (3, 0) NOT NULL
 	FOREIGN KEY (dept_name) REFERENCES Department
 );
 
 CREATE TABLE Instructor(
-	instructor_id CHAR(5) PRIMARY KEY,
-	instructor_name VARCHAR(20) NOT NULL,
+	ID CHAR(5) PRIMARY KEY,
+	[name] VARCHAR(20) NOT NULL,
 	dept_name VARCHAR(20) NOT NULL,
 	salary NUMERIC(8,2) NOT NULL
 	FOREIGN KEY (dept_name) REFERENCES Department
@@ -54,24 +54,24 @@ CREATE TABLE Section(
 );
 
 CREATE TABLE Teaches(
-	instructor_id CHAR(5),
+	ID CHAR(5),
 	course_id VARCHAR(8),
 	sec_id NUMERIC (1, 0),
 	semester VARCHAR(8),
 	[year] NUMERIC (4, 0)
-	PRIMARY KEY (instructor_id, course_id, sec_id, semester,  [year]),
-	FOREIGN KEY (instructor_id) REFERENCES Instructor,
+	PRIMARY KEY (ID, course_id, sec_id, semester,  [year]),
+	FOREIGN KEY (ID) REFERENCES Instructor,
 	FOREIGN KEY (course_id) REFERENCES Course
 );
 
 CREATE TABLE Takes (
-	student_id CHAR(5),
+	ID CHAR(5),
 	course_id VARCHAR(8),
 	sec_id NUMERIC (1, 0),
 	semester VARCHAR(8),
 	[year] NUMERIC(4,0),
 	grade VARCHAR(2),
-	primary KEY (student_id, course_id, sec_id, semester, [year]),
-	FOREIGN KEY (student_id) REFERENCES Students,
+	primary KEY (ID, course_id, sec_id, semester, [year]),
+	FOREIGN KEY (ID) REFERENCES Students,
 	FOREIGN KEY (course_id, sec_id, semester, [year]) REFERENCES Section 
 );
